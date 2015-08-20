32eedba90b5ec3122da049580b72f2bd6e2c8100
YangYouYong <yangyouyong@126.com>
2015-08-20 08:57:02 +0800

    更新patch

--- patch.js
+++ patch.js
@@ -2,7 +2,6 @@ defineClass("ViewController", {
   //instance method definitions
   tableView_didSelectRowAtIndexPath: function(tableView, indexPath) {
     var row = indexPath.row()
-    out.print("this is a patch method")
     print("this is a patch method")
   }
 }, {})
