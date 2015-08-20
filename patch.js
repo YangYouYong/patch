e3a9607fe1a2ef7730623ebe78b8f5f47536fe18
YangYouYong <yangyouyong@126.com>
2015-08-20 09:14:18 +0800

    更新patch代码

--- patch.js
+++ patch.js
@@ -2,7 +2,7 @@ defineClass("ViewController", {
   //instance method definitions
   tableView_didSelectRowAtIndexPath: function(tableView, indexPath) {
     var row = indexPath.row()
-    var alertView = require('UIAlertView').alloc().initWithTitle_message_delegate_cancelButtonTitle_otherButtonTitles("Alert",self.dataSource().objectAtIndex(indexPath.row()), self, "OK", null);
+    var alertView = require('UIAlertView').alloc().initWithTitle_message_delegate_cancelButtonTitle_otherButtonTitles("Alert","alert_message", null, "OK", null);
      alertView.show()
   }
 }, {})
