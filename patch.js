ac9ca1561110f7642d3a2a314b05c7aecfa40900
YangYouYong <yangyouyong@126.com>
2015-08-20 08:08:29 +0800

    更新代码

--- patch.js
+++ patch.js
@@ -1,7 +1,8 @@
-defineClass("UIViewController", {
+defineClass("ViewController", {
   //instance method definitions
   tableView_didSelectRowAtIndexPath: function(tableView, indexPath) {
     var row = indexPath.row()
     out.print("this is a patch method")
+    print("this is a patch method")
   }
 }, {})
