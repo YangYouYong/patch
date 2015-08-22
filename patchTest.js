f077cc404adb95423e0e318fca2294e6c7c2ed22
YangYouYong <yangyouyong@126.com>
2015-08-22 08:23:37 +0800

    change commite

--- patchTest.js
+++ patchTest.js
@@ -2,6 +2,6 @@ defineClass("PatchViewController",{
 	viewDidLoad: function() {
 	var self = this
 	this.view.backgroundColor = UIColor().greenColor
-	console.log("print view clolor")
+	log('print view clolor')
 	}
 },{})
