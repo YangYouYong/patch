7b2ab3dd8deec7dc80f2a696e4ea22836e886b07
YangYouYong <yangyouyong@126.com>
2015-08-22 09:21:34 +0800

    change color & alpha

--- patchTest.js
+++ patchTest.js
@@ -1,8 +1,7 @@
-defineClass("PatchViewController",{
-	var UIColor = require("UIColor") 
+defineClass("PatchViewController",{ 
 	viewDidLoad: function() {
 	var self = this
-	this.view.backgroundColor = UIColor.greenColor()
+	this.view.backgroundColor = require("UIColor").alloc().initWithRed_green_blue_alpha(0.5,0.5,0.5,1)
 	log('print view clolor')
 	}
 },{})
