b97623d353212ee66969285602ba7064e6be7813
YangYouYong <yangyouyong@126.com>
2015-08-22 09:07:47 +0800

    change bg clolor

--- patchTest.js
+++ patchTest.js
@@ -1,7 +1,7 @@
 defineClass("PatchViewController",{
 	viewDidLoad: function() {
 	var self = this
-	this.view.backgroundColor = Color().greenColor
+	this.view.backgroundColor = UIColor.greenColor()
 	log('print view clolor')
 	}
 },{})
