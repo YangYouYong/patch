f4a3f44a588b4401ea30db202fd8ad04931ec5fe
YangYouYong <yangyouyong@126.com>
2015-08-22 09:28:10 +0800

    new view

--- patchTest.js
+++ patchTest.js
@@ -1,7 +1,15 @@
 defineClass("PatchViewController",{ 
 	viewDidLoad: function() {
 	var self = this
-	this.view.backgroundColor = require("UIColor").alloc().initWithRed_green_blue_alpha(0.5,0.5,0.5,1)
+	// this.view.backgroundColor = require("UIColor").alloc().initWithRed_green_blue_alpha(0.5,0.5,0.5,1)
+
+	var myView = require("UIView").alloc().initWithFrame({
+		    x: 20,
+    		y: 100,
+    		width: 120,
+    		height: 20
+	})
+	self.view.addSubview(myView)
 	log('print view clolor')
 	}
 },{})
