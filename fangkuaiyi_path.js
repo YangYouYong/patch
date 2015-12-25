8a9defb8dfc1345d4b0036634268cccbe49145f2
yangyouyong <yangyouyong@111.com.cn>
2015-12-25 15:41:16 +0800

    [F] add dataSource for uncrash

--- fangkuaiyi_path.js
+++ fangkuaiyi_path.js
@@ -3,6 +3,7 @@ defineClass('FKYSearchResultViewController', {
 	// 	var self = this
 
 	// }
+	dataSource: function() {},
 	tableView_didSelectRowAtIndexPath: function(tableView, indexPath) {
 		var alertView = require('UIAlertView').alloc().initWithTitle_message_delegate_cancelButtonTitle_otherButtonTitles("Alert", self.dataSource().objectAtIndex(indexPath.row()), self, "OK", null);
 		alertView.show()
